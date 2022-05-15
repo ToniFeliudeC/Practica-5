1ebbd530378ef9774b99762821842477c7199722
ToniFeliu <91744505+ToniFeliudeC@users.noreply.github.com>
2022-05-15 19:48:33 +0200

    initial commit

---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+
+public class Calculadora implements ICalculadora {
+	
+	public float sumar(float a, float b) {
+		return (float)(a + b);
+	}
+	
+	public float restar(float a, float b) {
+		return (float)(a - b);
+	}
+	
+	public float multiplicar(float a, float b) {
+		return (float)(a * b);
+	}
+	
+	public float dividir(float a, float b) {
+		return (float)(a / b);
+	}
+}
---
+++ src/ICalculadora.java
@@ -0,0 +1,9 @@
+
+public interface ICalculadora {
+
+	public float sumar(float a, float b);
+	public float multiplicar(float a, float b);
+	public float restar(float a, float b);
+	public float dividir(float a, float b);
+	
+}
